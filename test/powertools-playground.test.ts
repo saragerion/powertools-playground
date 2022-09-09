1071c2094f218da359a37856a2fcc4f4f81e581a
Sara Gerion <sgerion@amazon.com>
2022-09-09 10:45:18 +0200

    Initial commit

---
+++ test/powertools-playground.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PowertoolsPlayground from '../lib/powertools-playground-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/powertools-playground-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PowertoolsPlayground.PowertoolsPlaygroundStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
