0883004d7f1942d464db8b053ab2f4e147611e1a
Sara Gerion <sgerion@amazon.com>
2022-09-09 11:39:24 +0200

    remove unused import

--- lib/powertools-playground.ts
+++ lib/powertools-playground.ts
@@ -1,7 +1,6 @@
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
-import {Duration} from "aws-cdk-lib";
 
 export class PowertoolsPlayground extends Construct {
     constructor(scope: Construct, id: string) {
